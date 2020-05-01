2267310611d2664c1faa9fd6d4960b1d72fdf097
iatk0s <iatkos@hotmail.com>
2020-05-01 01:37:21 -0500

    crear contacto

---
+++ contacto.php
@@ -0,0 +1,252 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="es">
+
+
+
+<head>
+	<link rel="shortcut icon" href="imagenes/favicon.png" > 
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>PC-INFORMATICA</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<script>window.jQuery || document.write('<script src="js/jquery-1.11.2.min.js"><\/script>')</script>
+	<script src="js/main.js" ></script>
+</head>
+
+
+
+
+<body>
+	
+	<nav class="NavBar full">
+		<div class="row">
+			<a href="index.php"><div class="tittles NavBar-logo col s12 m3">PC-INFORMATICA</div></a>
+			<ul class="NavBar-links col m9">
+				<li><a href="index.php" class="waves-effect">Inicio</a></li>
+				<li><a href="productos.php" class="waves-effect">Productos</a></li>
+				<li><a href="servicios.php" class="waves-effect ">Servicios</a></li>
+				<li><a href="ofertas.php" class="waves-effect ">Ofertas</a></li>
+				<li><a href="contacto.php" class="waves-effect ">Contacto</a></li>
+				<li><a href="ayuda.php" class="waves-effect">Ayuda</a></li>	
+				<li><a href="carrito.php" class="waves-effect "><img class="activator" src="./imagenes/carrito.png" width="40" height="40"></a></li>
+			</ul>
+			<i class="zmdi zmdi-more-vert full btn-menu hide-on-med-and-up waves-effect"></i>
+		</div>
+	</nav>
+
+
+
+
+
+	<div class="full ShoppingCart valign-wrapper">
+		<div class="container valign">
+			<div class="row valign-wrapper">
+				<div class="col s12 m6 valign">
+					<p class="center-align" style="color: #2F9FF3;">
+						<i class="zmdi zmdi-shopping-cart zmdi-hc-5x"></i>		
+					</p>
+					<p class="center-align">
+						<a class="btn-floating waves-effect waves-light red tooltipped" data-position="bottom" data-tooltip="Empty cart" style="margin-right: 17px;"><i class="zmdi zmdi-delete"></i></a>
+						<a class="btn-floating waves-effect waves-light green tooltipped" data-position="bottom" data-tooltip="Confirm purchase"><i class="zmdi zmdi-money"></i></a>
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+
+
+	<header class="full header valign-wrapper">
+		<div class="container valign">
+			<div class="row">
+				<div class="col s12 m6">
+					<figure>
+                                            <img src="imagenes/01.png"  alt="Header" class="responsive-img center-box">
+					</figure>
+				</div>
+				<div class="col s12 m6">
+					<h5 class="tittles center-align">PC-INFORMATICA</h5>
+					<p class="flow-text">
+						OFRECIENDO SIEMPRE LOS MEJORES PRODUCTOS Y SERVICOS DE VENTA Y REPARACION DE COMPUTADORAS, IMPRESORAS Y CONSUMIBLES SIEMPRE DE LA MEJOR CALIDAD.
+					</p>
+				</div>
+			</div>
+		</div>
+	</header>
+
+
+
+
+	<section class="full new-prod">
+		<h5 class="tittles center-align">CONTACTO</h5>
+		<div class="container">
+			<div class="row">
+				
+				<div class="col s12 m6 l4">
+					<div class="card hoverable">
+						<figure class="card-image">
+                                                    <img src="imagenes/c1.jpg"  width="150" height="180/">
+							
+						</figure>
+						
+					</div>
+				</div>	
+								<div class="col s12 m6 l4">
+					<div class="card hoverable">
+						<figure class="card-image">
+							<img src="imagenes/c2.jpg" width="150" height="180/" >
+							
+						</figure>
+						
+					</div>
+				</div>
+				<div class="col s12 m6 l4">
+					<div class="card hoverable">
+						<figure class="card-image">
+							<img src="imagenes/c3.jpg" width="150" height="180/">
+							
+						</figure>
+						
+						</div>
+					</div>
+					<div class="col s12 m6 l4">
+					<div class="card hoverable">
+						<figure class="card-image">
+							<img src="imagenes/c4.jpg" width="150" height="180/" >
+							
+						</figure>
+						
+						</div>
+					</div>
+					<div class="col s12 m6 l4">
+					<div class="card hoverable">
+						<figure class="card-image">
+							<img src="imagenes/c5.jpg" width="150" height="180/" >
+							
+						</figure>
+						
+						</div>
+					</div>
+					<div class="col s12 m6 l4">
+					<div class="card hoverable">
+						<figure class="card-image">
+							<img src="imagenes/c6.jpg" width="150" height="180/" >
+							
+						</figure>
+						
+						</div>
+					</div>
+				</div>
+			</div>
+<h6 class="tittles center-align">Telefono:  (01) 756-104-10-28</h6>
+<h6 class="tittles center-align">Correo electronico: pc_informatica@gmail.com</h6>
+<h6 class="tittles center-align">WhatsApp: (+52) 756-104-10-28</h6>
+		</section>
+	<section class="full promo">
+		<div class="container">
+			<div class="row">
+				<div class="col s12">
+					<h5 class="tittles center-align">Encuentranos en...</h5>
+				</div>
+			    <div class="col s12 m4">
+
+			    	<p class="center-align">
+			        	
+							<img src="imagenes/facebook.jpg" width="250" height="250/">
+							
+			    	</p>
+			        <p class="center-align flow-text">
+			        Facebook</p>
+			        <p>
+			        	Mantente al pendiente de todas nuestras publicasiones y nuevo material.
+			        </p>
+			    </div>
+			    <div class="col s12 m4">
+			    	<p class="center-align">
+			        	
+                                    <img src="imagenes/twitter.png" width="250" height="250/"> 			
+						
+			    	</p>
+			        <p class="center-align flow-text">Twitter</p>
+			        <p>
+			        	Estaremos subiendo fotos de nuestro material mas reciente e inovador.
+			        </p>
+			    </div>
+			    <div class="col s12 m4">
+			    	<p class="center-align">
+			        			        	
+                                    <img src="imagenes/google.png" width="250" height="250/">					
+						
+			    	</p>
+			        <p class="center-align flow-text">Google</p>
+			        <p>
+			        	Encuentra nuestras instalaciones con mayor facilidad en Maps. 
+			        </p>
+			    </div>
+			    
+			    <div class="col s12">
+					<h5 class="tittles center-align">Maps</h5>
+				</div>
+				<p class="center-align">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d950.7723940574401!2d-99.1803525955751!3d17.59848076889438!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e0!4m0!4m5!1s0x85c9587d3f314527%3A0xf3a18001bff6edc4!2sCompu+Reparaci%C3%B3n+%22La+Villa%22%2C+Calle+13+Nte.+727%2C+Progreso%2C+41100+Chilapa+de+%C3%81lvarez%2C+Gro.!3m2!1d17.5986247!2d-99.1798932!5e0!3m2!1ses-419!2smx!4v1566209876540!5m2!1ses-419!2smx" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+			</p>
+		    </div>
+		</div>	
+	</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	<footer class="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col s12 m6">
+					<h5>Quienes somos?...</h5>
+					<p>
+						Somos una empresa dedicada a la reparación y venta de diversos equipos de computo, todo con los más altos estandares de calidad.
+					</p>
+				</div>
+				<div class="col s12 m6">
+					<h5 class="center-align">Contactanos!...</h5>
+					<ul class="center-align">
+						<li><a href="#!"><i class="zmdi zmdi-facebook"></i> &nbsp; Facebook</a></li>
+						<li><a href="#!"><i class="zmdi zmdi-twitter"></i> &nbsp; Twitter</a></li>
+						<li><a href="#!"><i class="zmdi zmdi-google-plus"></i> &nbsp; Google</a></li>
+						
+					</ul>	
+				</div>
+				<div class="col s12 tittles footer-copyright">PC-INFORMATICA &copy; 2019</div>
+			</div>
+		</div>
+	</footer>
+	<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5defea0ed96992700fcbaac7/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-
+tawk.to
+</body>
+</html>
