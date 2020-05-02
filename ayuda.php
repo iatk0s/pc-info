d20df57459f4c2a8600cab14336bad8bcecb368e
iatk0s <iatkos@hotmail.com>
2020-05-02 02:44:12 -0500

    Create ayuda.php

---
+++ ayuda.php
@@ -0,0 +1,166 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="es">
+
+
+
+<head>
+	<link rel="shortcut icon" href="imagenes/favicon.png" > 
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>AYUDA</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<script>window.jQuery || document.write('<script src="js/jquery-1.11.2.min.js"><\/script>')</script>
+	<script src="js/main.js" ></script>
+</head>
+
+
+
+
+<body>
+	
+	<nav class="NavBar full">
+		<div class="row">
+			<a href="index.php"><div class="tittles NavBar-logo col s12 m3">PC-INFORMATICA</div></a>
+			<ul class="NavBar-links col m9">
+				<li><a href="index.php" class="waves-effect">Inicio</a></li>
+				<li><a href="productos.php" class="waves-effect">Productos</a></li>
+				<li><a href="servicios.php" class="waves-effect ">Servicios</a></li>
+				<li><a href="ofertas.php" class="waves-effect ">Ofertas</a></li>
+				<li><a href="contacto.php" class="waves-effect ">Contacto</a></li>
+				<li><a href="ayuda.php" class="waves-effect">Ayuda</a></li>	
+				<li><a href="carrito.php" class="waves-effect "><img class="activator" src="imagenes/carrito.png" width="40" height="40"></a></li>
+			</ul>
+			<i class="zmdi zmdi-more-vert full btn-menu hide-on-med-and-up waves-effect"></i>
+		</div>
+	</nav>
+
+
+
+
+
+	<div class="full ShoppingCart valign-wrapper">
+		<div class="container valign">
+			<div class="row valign-wrapper">
+				<div class="col s12 m6 valign">
+					<p class="center-align" style="color: #2F9FF3;">
+						<i class="zmdi zmdi-shopping-cart zmdi-hc-5x"></i>		
+					</p>
+					<p class="center-align">
+						<a class="btn-floating waves-effect waves-light red tooltipped" data-position="bottom" data-tooltip="Empty cart" style="margin-right: 17px;"><i class="zmdi zmdi-delete"></i></a>
+						<a class="btn-floating waves-effect waves-light green tooltipped" data-position="bottom" data-tooltip="Confirm purchase"><i class="zmdi zmdi-money"></i></a>
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+
+
+
+
+
+<h2>¿Cómo   te puedo ayudar?</h2>
+...............................................................................................................................................................
+
+<h4>
+	¿ desea comprar?
+</h4>
+
+<P>claro!</P>
+
+
+<h4>Configuración de cuenta
+	
+</h3>
+
+ 
+<p>
+	Por si buscabas ingresar una cuenta este sitio web no es para que te registres es ver y comprar los productos que te ofrecemos y puedas adquirirlos en el lugar donde se encuentra ubicado el negocio o atraves de este sitio.
+</p>
+<h4>
+	Seguridad.
+</h4>
+
+	<p>	Si buscabas algún tipo de seguridad no te lo proporcionamos esto sería por si fuera entrega a domicilio pero como no tenemos eso no te proporcionamos seguridad 
+		ya que solo vendrías a comprar hasta el negocio.
+
+	</p>
+
+
+<h4>¿No encuentras la información que buscabas?</h4>
+<p>Escríbenos a  nuestras redes antes mencionadas y te responderemos al instante.</p>
+
+
+<p></p>
+
+
+
+
+	<section class="full promo">
+		<div class="container">
+			<div class="row">
+				<div class="col s12">
+					<h5 class="tittles center-align">Beneficios</h5>
+				</div>
+			    <div class="col s12 m4">
+			    	<p class="center-align">
+			        	<i class="zmdi zmdi-card-giftcard zmdi-hc-5x"></i>
+			    	</p>
+			        <p class="center-align flow-text">Grandes Regalos</p>
+			        <p>
+			        	Recibe regalos haciendo compras en la tienda.
+			        </p>
+			    </div>
+			    <div class="col s12 m4">
+			    	<p class="center-align">
+			        	<i class="zmdi zmdi-card zmdi-hc-5x"></i>
+			    	</p>
+			        <p class="center-align flow-text">Muchas formas de pago</p>
+			        <p>
+			        	Aceptamos pagos con tarjetas de credito y debito.
+			        </p>
+			    </div>
+			    <div class="col s12 m4">
+			    	<p class="center-align">
+			        	<i class="zmdi zmdi-money zmdi-hc-5x"></i>
+			    	</p>
+			        <p class="center-align flow-text">Descuentos Exclusivos</p>
+			        <p>
+			        	Opten los mejores descuentos de la temporada. 
+			        </p>
+			    </div>
+		    </div>
+		</div>	
+	</section>
+
+
+
+
+	<footer class="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col s12 m6">
+					<h5>Quienes somos?...</h5>
+					<p>
+						Somos una empresa dedicada a la reparacion y venta de diversos equipos de computo, todo con los más altos estandares de calidad.
+					</p>
+				</div>
+				<div class="col s12 m6">
+					<h5 class="center-align">Contactanos!...</h5>
+					<ul class="center-align">
+						<li><a href="#!"><i class="zmdi zmdi-facebook"></i> &nbsp; Facebook</a></li>
+						<li><a href="#!"><i class="zmdi zmdi-twitter"></i> &nbsp; Twitter</a></li>
+						<li><a href="#!"><i class="zmdi zmdi-google-plus"></i> &nbsp; Google</a></li>
+						<li><a href="/login/index.php"><i class=""></i> &nbsp; log/reg</a></li>
+						
+					</ul>	
+				</div>
+				<div class="col s12 tittles footer-copyright">PC-INFORMATICA &copy; 2019</div>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
